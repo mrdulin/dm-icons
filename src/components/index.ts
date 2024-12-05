024e2beb312db31a29c4a3dbfb81fd6a8c781fb8
mrdulin <novaline.dulin@gmail.com>
2024-12-05 17:51:20 +0800

    feat: initial commit

---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+export { default as HasCut } from "./has-cut";
+export { default as React } from "./react";
+export { default as Search } from "./search";
---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+/// <reference types="vite/client" />
+import { defineConfig } from 'vite';
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), dts({ tsconfigPath: './tsconfig.app.json' })],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/components/index.ts'),
+      formats: ['es', 'cjs'],
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
+      output: {
+        entryFileNames: '[name].js',
+        preserveModules: true,
+        globals: {
+          react: 'React',
+          'react-dom': 'React-dom',
+          'react/jsx-runtime': 'react/jsx-runtime',
+        },
+      },
+    },
+  },
+});
