056354d5b0e65694cf5cfd0c276d4095d209cacd
mrdulin <novaline.dulin@gmail.com>
2024-12-13 19:07:13 +0800

    feat:  修改vite.config，tranform rest spread operator 语法

--- packages/icons-react/vite.config.ts
+++ packages/icons-react/vite.config.ts
@@ -3,11 +3,14 @@ import { defineConfig } from 'vite';
 import path from 'path';
 import react from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
+import resolve from '@rollup/plugin-node-resolve';
+import { babel } from '@rollup/plugin-babel';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), dts({ tsconfigPath: './tsconfig.app.json', rollupTypes: true })],
   build: {
+    emptyOutDir: true,
     lib: {
       entry: path.resolve(__dirname, 'src/components/index.ts'),
       formats: ['es', 'cjs'],
@@ -21,6 +24,15 @@ export default defineConfig({
           'react/jsx-runtime': 'react/jsx-runtime',
         },
       },
+      plugins: [
+        resolve(),
+        babel({
+          exclude: 'node_modules',
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+          babelHelpers: 'bundled',
+          plugins: [['@babel/plugin-transform-object-rest-spread', { useBuiltIns: true }]],
+        }),
+      ],
     },
   },
 });
