627f30b00769dc79c15a4ed9d2889579f86b7a67
mrdulin <novaline.dulin@gmail.com>
2024-12-13 17:52:16 +0800

    feat:  修改vite.config，合并d.ts为一个文件

--- packages/icons-react/vite.config.ts
+++ packages/icons-react/vite.config.ts
@@ -6,7 +6,7 @@ import dts from 'vite-plugin-dts';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ tsconfigPath: './tsconfig.app.json' })],
+  plugins: [react(), dts({ tsconfigPath: './tsconfig.app.json', rollupTypes: true })],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/components/index.ts'),
