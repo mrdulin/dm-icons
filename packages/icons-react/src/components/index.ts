129b1cf5b284c99e8e0b08261e100434b0aacbab
mrdulin <novaline.dulin@gmail.com>
2024-12-16 14:58:52 +0800

    feat:  添加3个svg；更新本地link npm script

--- packages/icons-react/src/components/index.ts
+++ packages/icons-react/src/components/index.ts
@@ -1,4 +1,7 @@
 export { default as Excel } from "./excel";
+export { default as Flag } from "./flag";
 export { default as HasCut } from "./has-cut";
 export { default as Pdf } from "./pdf";
+export { default as Pixels } from "./pixels";
 export { default as Search } from "./search";
+export { default as Server } from "./server";
