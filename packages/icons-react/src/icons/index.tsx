59d99ae2a67afffcee80f26c91aa454b57714e8b
mrdulin <novaline.dulin@gmail.com>
2024-12-24 17:04:15 +0800

    feat: generate entry file for icon components

---
+++ packages/icons-react/src/icons/index.tsx
@@ -0,0 +1,18 @@
+// GENERATE BY ./scripts/generate.ts
+      // DON NOT EDIT IT MANUALLY
+      export { default as Excel } from './Excel';
+export { default as Expand } from './Expand';
+export { default as Flag } from './Flag';
+export { default as HasCut } from './HasCut';
+export { default as Hot } from './Hot';
+export { default as Item } from './Item';
+export { default as LineChart } from './LineChart';
+export { default as Pdf } from './Pdf';
+export { default as Pixels } from './Pixels';
+export { default as Rollback } from './Rollback';
+export { default as Search } from './Search';
+export { default as Server } from './Server';
+export { default as Shrink } from './Shrink';
+export { default as Target } from './Target';
+export { default as ThumbsDown } from './ThumbsDown';
+export { default as ThumbsUp } from './ThumbsUp';
