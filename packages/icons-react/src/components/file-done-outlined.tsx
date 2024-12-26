f3e46008ce05a8c464b11c33fc783b1790e0b137
mrdulin <novaline.dulin@gmail.com>
2024-12-26 16:09:45 +0800

    feat: add more icons;

---
+++ packages/icons-react/src/components/file-done-outlined.tsx
@@ -0,0 +1,26 @@
+import type { SVGProps } from "react";
+interface SVGRProps {
+  title?: string;
+  titleId?: string;
+}
+const SvgFileDoneOutlined = ({
+  title,
+  titleId,
+  ...props
+}: SVGProps<SVGSVGElement> & SVGRProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 16 16"
+    width="1em"
+    height="1em"
+    fill="currentColor"
+    aria-hidden="true"
+    focusable="false"
+    aria-labelledby={titleId}
+    {...props}
+  >
+    {title ? <title id={titleId}>{title}</title> : null}
+    <path d="M12.964 5.165A6.5 6.5 0 0 0 6.81 16H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h4.662a2 2 0 0 1 1.286.469 399 399 0 0 1 4.613 3.934q.311.27.403.762M3 3a1 1 0 0 0 0 2h4a1 1 0 0 0 0-2zm0 4a1 1 0 0 0 0 2h1a1 1 0 0 0 0-2zm11.695 1.33-3.345 3.307-1.252-1.22a.812.812 0 1 0-1.134 1.164l1.822 1.777a.81.81 0 0 0 1.139-.004l3.7-3.657a4.5 4.5 0 1 1-.93-1.366" />
+  </svg>
+);
+export default SvgFileDoneOutlined;
---
+++ packages/icons-react/src/components/info-circle-filled.tsx
@@ -0,0 +1,26 @@
+import type { SVGProps } from "react";
+interface SVGRProps {
+  title?: string;
+  titleId?: string;
+}
+const SvgInfoCircleFilled = ({
+  title,
+  titleId,
+  ...props
+}: SVGProps<SVGSVGElement> & SVGRProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 16 16"
+    width="1em"
+    height="1em"
+    fill="currentColor"
+    aria-hidden="true"
+    focusable="false"
+    aria-labelledby={titleId}
+    {...props}
+  >
+    {title ? <title id={titleId}>{title}</title> : null}
+    <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0m1.619 12.006c.213 0 .386-.201.386-.448s-.173-.448-.386-.448L9 11V7c0-.041.004-.055.007-.07-.011.026-.036.058-.113.058H6.38c-.213 0-.385.201-.385.448s.172.448.385.448L7 8v3l-.62.11c-.213 0-.385.201-.385.448s.172.448.385.448zM7 4v.941c-.002.021-.013.039-.013.06 0 .022.011.039.013.06V6h.967c.011.001.021.007.033.007.011 0 .021-.006.033-.007H9v-.938c.001-.021.012-.039.012-.061S9.001 4.962 9 4.94V4h-.98c-.007 0-.013-.004-.02-.004S7.987 4 7.98 4z" />
+  </svg>
+);
+export default SvgInfoCircleFilled;
