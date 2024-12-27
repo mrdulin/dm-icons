bc456fc9d56d5ebc71c9ffa51c3f9fbe23a3369c
mrdulin <novaline.dulin@gmail.com>
2024-12-27 18:15:41 +0800

    fix: split development and production vite config

--- packages/icons-site/vite.config.ts
+++ packages/icons-site/vite.config.ts
@@ -2,16 +2,29 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/dm-icons/',
-  resolve: {
-    preserveSymlinks: true,
-  },
-  optimizeDeps: { exclude: ['@d-matrix/icons-react'] },
-  // build: {
-  //   commonjsOptions: {
-  //     include: ['@d-matrix/icons-react'],
-  //   },
-  // },
+export default defineConfig(({ command }) => {
+  if (command === 'serve') {
+    return {
+      plugins: [react()],
+      base: '/dm-icons/',
+      // resolve: {
+      //   preserveSymlinks: true,
+      // },
+      optimizeDeps: { include: ['@d-matrix/icons-react'] },
+      build: {
+        commonjsOptions: {
+          include: ['@d-matrix/icons-react'],
+        },
+      },
+    };
+  } else {
+    return {
+      plugins: [react()],
+      base: '/dm-icons/',
+      // resolve: {
+      //   preserveSymlinks: true,
+      // },
+      optimizeDeps: { exclude: ['@d-matrix/icons-react'] },
+    };
+  }
 });
