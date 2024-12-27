e1f604053a48b5f0dce034384710993755aef6fb
mrdulin <novaline.dulin@gmail.com>
2024-12-27 14:13:33 +0800

    feat: add more icons

---
+++ packages/icons-react/src/components/leaf-outlined.tsx
@@ -0,0 +1,26 @@
+import type { SVGProps } from "react";
+interface SVGRProps {
+  title?: string;
+  titleId?: string;
+}
+const SvgLeafOutlined = ({
+  title,
+  titleId,
+  ...props
+}: SVGProps<SVGSVGElement> & SVGRProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 16 16"
+    width="1em"
+    height="1em"
+    fill="currentColor"
+    aria-hidden="true"
+    focusable="false"
+    aria-labelledby={titleId}
+    {...props}
+  >
+    {title ? <title id={titleId}>{title}</title> : null}
+    <path d="M.123 13.127C1.92 9.972 4.017 7.069 8.83 3.887c.221-.148.047-.495-.201-.4C4.98 4.904 3.444 7.026 2.17 8.311a.41.41 0 0 1-.658-.106C.898 7.014-.058 4.198 3.71 2.148 7.046.334 10.936.044 13.416 0c.427-.007.712.447.526.838-.695 1.47-1.172 3.726-2.362 5.789-2.465 4.271-5.52 4.584-8.306 3.504a.45.45 0 0 0-.542.178c-.375.58-1.136 1.844-1.326 2.818-.257 1.308-1.856 1.01-1.283 0" />
+  </svg>
+);
+export default SvgLeafOutlined;
