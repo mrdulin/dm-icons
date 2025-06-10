4d2067ae38d2c5478bf75d440337f83b71fe600a
mrdulin <novaline.dulin@gmail.com>
2025-06-10 14:19:25 +0800

    feat: add more icons

--- packages/icons-react/src/icons/PdfSquareFilled.tsx
+++ packages/icons-react/src/icons/PdfSquareFilled.tsx
@@ -4,7 +4,7 @@
       import React from 'react';
       import { DMIcon, DMIconProps } from '../dm-icon';
 
-      const PdfSquareColored = (props: Omit<DMIconProps, 'icon'>) => {
+      const PdfSquareFilled = (props: Omit<DMIconProps, 'icon'>) => {
         return <DMIcon {...props} icon={
           // @ts-expect-error TODO improve TS type
           ({ title, titleId, ...props }) => (React.createElement("svg", { xmlns: "http://www.w3.org/2000/svg", viewBox: "0 0 16 16", width: "1em", height: "1em", fill: "currentColor", "aria-hidden": "true", focusable: "false", "aria-labelledby": titleId, ...props },
@@ -16,4 +16,4 @@
         } />;
       }
 
-      export default PdfSquareColored;
+      export default PdfSquareFilled;
