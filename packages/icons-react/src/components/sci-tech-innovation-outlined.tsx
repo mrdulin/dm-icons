42443ca70ba9d991ef36389a8356fcd07b17887c
mrdulin <novaline.dulin@gmail.com>
2025-06-17 14:17:56 +0800

    feat: add more icons

---
+++ packages/icons-react/src/components/sci-tech-innovation-outlined.tsx
@@ -0,0 +1,33 @@
+import type { SVGProps } from "react";
+interface SVGRProps {
+  title?: string;
+  titleId?: string;
+}
+const SvgSciTechInnovationOutlined = ({
+  title,
+  titleId,
+  ...props
+}: SVGProps<SVGSVGElement> & SVGRProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 12 12"
+    width="1em"
+    height="1em"
+    fill="currentColor"
+    aria-hidden="true"
+    focusable="false"
+    aria-labelledby={titleId}
+    {...props}
+  >
+    {title ? <title id={titleId}>{title}</title> : null}
+    <g fill="none" fillRule="evenodd" transform="translate(-2 -2)">
+      <rect width={15} height={15} x={0.5} y={0.5} stroke="#666" rx={2} />
+      <path
+        fill="currentColor"
+        fillRule="nonzero"
+        d="M10.753 13.5v-2.905l-3.936.354-.113-1.156 4.049-.366V2.5h1.201v6.816l1.48-.133.066 1.156-1.546.134V13.5zm-6.47-.012V8.91a14.8 14.8 0 0 1-1.385 2.627L2.5 10.17q1.053-1.435 1.67-3.3H2.626V5.748h1.659v-1.51a41 41 0 0 1-1.403.092 9 9 0 0 0-.173-1.121q1.777-.07 4.097-.343l.226 1.087q-.839.122-1.588.174v1.621h1.499V6.87H5.443v1.272l.333-.377q.084.063.262.209.88.697 1.225 1l-.767.842a25 25 0 0 0-.726-.68l-.327-.296v4.648zm4.775-4.735a43 43 0 0 0-1.908-1.61l.731-.796q.981.732 1.968 1.528zM9.63 5.98a45 45 0 0 0-1.956-1.685l.731-.79a56 56 0 0 1 2.016 1.62z"
+      />
+    </g>
+  </svg>
+);
+export default SvgSciTechInnovationOutlined;
