561fbbd0bc31d240cea8ce527576cf0a13288f42
mrdulin <novaline.dulin@gmail.com>
2025-11-19 14:27:29 +0800

    feat(icons-react): 添加 EyeOutlined2 图标组件
    新增 EyeOutlined2 SVG 图标及其对应的 React 组件实现，并在图标导出入口文件中完成注册。该图标包含完整的可访问性支持，如 title 属性和 aria 标签功能。

---
+++ packages/icons-react/src/icons/EyeOutlined2.tsx
@@ -0,0 +1,16 @@
+// GENERATE BY ./scripts/generate.ts
+    // DON NOT EDIT IT MANUALLY
+
+      import React from 'react';
+      import { DMIcon, DMIconProps } from '../dm-icon';
+
+      const EyeOutlined2 = (props: Omit<DMIconProps, 'icon'>) => {
+        return <DMIcon {...props} icon={
+          // @ts-expect-error TODO improve TS type
+          ({ title, titleId, ...props }) => (React.createElement("svg", { xmlns: "http://www.w3.org/2000/svg", fill: "currentColor", viewBox: "0 0 14 14", width: "1em", height: "1em", "aria-hidden": "true", focusable: "false", "aria-labelledby": titleId, ...props },
+    title ? React.createElement("title", { id: titleId }, title) : null,
+    React.createElement("path", { fill: "currentColor", d: "M1.228 7A6.128 6.128 0 0 0 12.78 7 6.128 6.128 0 0 0 1.228 7M.057 6.823a7.295 7.295 0 0 1 13.895 0l.057.177-.057.177a7.295 7.295 0 0 1-13.895 0L0 7zM7.004 5.25a1.75 1.75 0 1 0 0 3.5 1.75 1.75 0 0 0 0-3.5M4.088 7A2.917 2.917 0 1 1 9.92 7a2.917 2.917 0 0 1-5.833 0" })))
+        } />;
+      }
+
+      export default EyeOutlined2;
