feb829dfb38f9e58c2c289143dad7a5b2597ea8e
mrdulin <novaline.dulin@gmail.com>
2025-12-18 14:21:02 +0800

    feat(icons-react): 更新图标命名及填充色以支持主题定制
    将 `AlertColored` 和 `ViewpointColored` 图标重命名为 `AlertFilled` 和 `ViewpointFilled`，
    并将其内部填充颜色由硬编码的 `#F0BB0E` 改为 `currentColor`，以便支持通过 CSS 主题色进行统一控制。
    
    同时，完善了 README 中关于发布流程的说明，并调整部分 SVG 组件属性顺序以提升一致性。

--- packages/icons-react/src/components/enlarge-outlined.tsx
+++ packages/icons-react/src/components/enlarge-outlined.tsx
@@ -12,8 +12,8 @@ const SvgEnlargeOutlined = ({
     xmlns="http://www.w3.org/2000/svg"
     width="1em"
     height="1em"
-    fill="currentColor"
     viewBox="0 0 16 16"
+    fill="currentColor"
     aria-hidden="true"
     focusable="false"
     aria-labelledby={titleId}
@@ -22,6 +22,7 @@ const SvgEnlargeOutlined = ({
     {title ? <title id={titleId}>{title}</title> : null}
     <path d="M12.581 7.158h-1.2v-1.69L8.13 8.722l-.424-.425-.424-.424 3.252-3.252h-1.69v-1.2h3.737z" />
     <path
+      fillRule="evenodd"
       d="M14.982 14.982H1.017V1.017h13.965zM2.217 9.402h2.709c.966 0 1.75.783 1.75 1.75v2.63h7.106V2.217H2.217z"
       clipRule="evenodd"
     />
